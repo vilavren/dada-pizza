9e4da7c11cf3dfe6a2fcd36a1b0fcdadbf14fb67
vitalii <vilavren@gmail.com>
2022-10-02 18:29:31 +0500

    division into components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import './App.css'
+import './scss/app.scss'
+import Card from './scss/components/Card'
+import Categories from './scss/components/Categories'
+import Header from './scss/components/Header'
+import Sort from './scss/components/Sort'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <div class="wrapper">
+        <Header />
+        <div class="content">
+          <div class="container">
+            <div class="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 class="content__title">Все пиццы</h2>
+            <div class="content__items">
+              <Card title="Мексиканская" price={395} />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
