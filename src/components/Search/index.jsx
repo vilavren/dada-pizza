b1536a549a71c3e4432769f4fa3d5066bf9120d0
vitalii <vilavren@gmail.com>
2022-10-13 16:50:50 +0500

    added delayed search on the back

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,9 +1,29 @@
-import { useContext } from 'react'
+import { useCallback, useContext, useRef, useState } from 'react'
+import debounce from 'lodash.debounce'
+
 import { SearchContext } from '../../App'
 import styles from './Search.module.scss'
 
 function Search() {
-  const { searchValue, setSearchValue } = useContext(SearchContext)
+  const [value, setValue] = useState('')
+  const { setSearchValue } = useContext(SearchContext)
+  const inputRef = useRef()
+
+  const onClickClear = () => {
+    setValue('')
+    setSearchValue('')
+    inputRef.current.focus()
+  }
+
+  const updateSearchValue = useCallback(
+    debounce((str) => setSearchValue(str), 150),
+    []
+  )
+
+  const onChangeInput = (event) => {
+    setValue(event.target.value)
+    updateSearchValue(event.target.value)
+  }
 
   return (
     <div className={styles.root}>
@@ -42,14 +62,15 @@ function Search() {
         />
       </svg>
       <input
-        value={searchValue}
-        onChange={(event) => setSearchValue(event.target.value)}
+        ref={inputRef}
+        value={value}
+        onChange={onChangeInput}
         className={styles.input}
         placeholder="Поиск пиццы..."
       />
-      {searchValue && (
+      {value && (
         <svg
-          onClick={() => setSearchValue('')}
+          onClick={onClickClear}
           className={styles.clearIcon}
           height="512px"
           id="Layer_1"
