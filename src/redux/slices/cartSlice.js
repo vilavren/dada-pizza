bcfdc4b259deb676501f5f745f69d976d778e81a
vitalii <vilavren@gmail.com>
2022-10-18 01:58:34 +0500

    added shopping cart functionality

---
+++ src/redux/slices/cartSlice.js
@@ -0,0 +1,50 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  totalPrice: 0,
+  items: [],
+}
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      const findItem = state.items.find((obj) => obj.id === action.payload.id)
+
+      if (findItem) {
+        findItem.count++
+      } else {
+        state.items.push({
+          ...action.payload,
+          count: 1,
+        })
+      }
+
+      state.totalPrice = state.items.reduce((sum, item) => {
+        return item.price * item.count + sum
+      }, 0)
+    },
+
+    minusItem(state, action) {
+      const findItem = state.items.find((obj) => obj.id === action.payload)
+
+      if (findItem) {
+        findItem.count--
+      }
+    },
+
+    removeItem(state, action) {
+      state.items = state.items.filter((obj) => obj.id === action.payload.id)
+    },
+
+    clearItem(state) {
+      state.items = []
+      state.totalPrice = 0
+    },
+  },
+})
+
+export const { addItem, removeItem, minusItem, clearItem } = cartSlice.actions
+
+export default cartSlice.reducer
