245ba318ac2acb392effd052c79a450000accad8
vitalii <vilavren@gmail.com>
2022-10-17 16:40:04 +0500

    added hiding sorting by click in another area

--- src/components/Sort/index.jsx
+++ src/components/Sort/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setSort } from '../../redux/slices/filterSlice'
 
@@ -13,6 +13,7 @@ const list = [
 function Sort() {
   const dispatch = useDispatch()
   const sort = useSelector((state) => state.filter.sort)
+  const sortRef = useRef()
 
   const [open, setOpen] = useState(false)
 
@@ -20,8 +21,22 @@ function Sort() {
     dispatch(setSort(obj))
     setOpen(false)
   }
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (!e.path.includes(sortRef.current)) {
+        setOpen(false)
+        console.log('ntnc')
+      }
+    }
+
+    document.body.addEventListener('click', handleClickOutside)
+
+    return () => document.body.removeEventListener('click', handleClickOutside)
+  }, [])
+
   return (
-    <div className="sort">
+    <div ref={sortRef} className="sort">
       <div className="sort__label">
         <svg
           width="10"
